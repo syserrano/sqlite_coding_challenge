4fb4e4cff34589c7008308da1b8ac88230bf87e2
Sofia Serrano <syserrano@usf.edu>
2026-04-19 23:01:05 -0400

    Revenue by product category

--- challenge.session.sql
+++ challenge.session.sql
@@ -12,18 +12,3 @@ GROUP BY
 ORDER BY
     total_spent DESC
 LIMIT 5;
-
-
-SELECT
-    p.category,
-    ROUND(SUM(oi.quantity * oi.unit_price), 2) AS total_revenue
-FROM
-    order_items oi
-JOIN
-    products p ON oi.product_id = p.product_id
-GROUP BY
-    p.category
-ORDER BY
-    total_revenue DESC
-
-
